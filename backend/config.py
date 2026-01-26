cfbf99cdc82d4e0f4769883d36cc3926a2b85bf2
Mariam Husain <66348441+mariam-hedgie@users.noreply.github.com>
2026-01-26 10:23:00 -0500

    config and requirements

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+SAMPLE_RATE_HERTZ = 256 # samples/sec our "neural device" produces
+NUM_CHANNELS = 4 # num of simualted EEG channels
+DB_PATH = "neurostream.db" # path to SQLite database file
+BUFFER_SIZE = 500 # how many recent samples returned by API for visualization
